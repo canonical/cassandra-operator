cc70d08fb60ba9aefd6a219064a4fbb8d73edef6
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2025-06-19 12:42:06 +0300

    Use jubilant instead of pytest-operator.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,13 +2,11 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
 import logging
 from pathlib import Path
 
-import pytest
+import jubilant
 import yaml
-from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
@@ -16,23 +14,10 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 
 
-@pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
-
-    Assert on the unit status before any relations/configurations take place.
-    """
-    # Build and deploy charm from local source folder
-    charm = await ops_test.build_charm(".")
-
-    # Deploy the charm and wait for active/idle status
-    await asyncio.gather(
-        ops_test.model.deploy(
-            charm,
-            application_name=APP_NAME,
-            config={"profile": "testing"},
-        ),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-        ),
+def test_deploy(juju: jubilant.Juju, cassandra_charm: Path) -> None:
+    juju.deploy(
+        cassandra_charm,
+        app=APP_NAME,
+        config={"profile": "testing"},
     )
+    juju.wait(jubilant.all_active)
