5ec428f3dec94595924c07dd14c680af18eabc46
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2025-06-26 15:18:04 +0300

    Multinode deployment.

--- src/events/cassandra.py
+++ src/events/cassandra.py
@@ -53,45 +53,57 @@ class CassandraEvents(Object):
 
     def _on_install(self, _: InstallEvent) -> None:
         self.workload.install()
-        self.state.unit.workload_state = UnitWorkloadState.STARTING.value
+        self.state.unit.workload_state = UnitWorkloadState.STARTING
 
     def _on_start(self, event: StartEvent) -> None:
         self._update_network_address()
-        try:
-            self.config_manager.render_env(
-                cassandra_limit_memory_mb=1024 if self.charm.config.profile == "testing" else None
-            )
-        except ValidationError as e:
-            logger.debug(f"Config haven't passed validation: {e}")
-            event.defer()
-            return
 
-        if (
-            not self.charm.unit.is_leader()
-            and self.state.cluster.state != ClusterState.ACTIVE.value
-        ):
+        if not self.charm.unit.is_leader() and self.state.cluster.state != ClusterState.ACTIVE:
             logger.debug("Deferring on_start for unit due to cluster isn't initialized yet")
             event.defer()
             return
 
-        self.workload.restart()
-        self.state.unit.workload_state = UnitWorkloadState.ACTIVE.value
         if self.charm.unit.is_leader():
-            self.state.cluster.state = ClusterState.ACTIVE.value
+            self.state.cluster.seeds = [self.state.unit.peer_url]
 
-    def _on_config_changed(self, event: ConfigChangedEvent) -> None:
         try:
             self.config_manager.render_env(
                 cassandra_limit_memory_mb=1024 if self.charm.config.profile == "testing" else None
             )
+            self.config_manager.render_cassandra_config(
+                cluster_name=self.charm.config.cluster_name,
+                listen_address=self.state.unit.ip,
+                seeds=self.state.cluster.seeds,
+            )
+        except ValidationError as e:
+            logger.debug(f"Config haven't passed validation: {e}")
+            event.defer()
+            return
+
+        self.workload.start()
+        self.state.unit.workload_state = UnitWorkloadState.ACTIVE
+
+        if self.charm.unit.is_leader():
+            self.state.cluster.state = ClusterState.ACTIVE
+
+    def _on_config_changed(self, _: ConfigChangedEvent) -> None:
+        try:
+            self.config_manager.render_env(
+                cassandra_limit_memory_mb=1024 if self.charm.config.profile == "testing" else None
+            )
+            self.config_manager.render_cassandra_config(
+                cluster_name=self.charm.config.cluster_name,
+                listen_address=self.state.unit.ip,
+                seeds=self.state.cluster.seeds,
+            )
         except ValidationError as e:
             logger.debug(f"Config haven't passed validation: {e}")
             return
-        if not self.state.unit.workload_state == UnitWorkloadState.ACTIVE.value:
+        if not self.state.unit.workload_state == UnitWorkloadState.ACTIVE:
             return
         self.workload.restart()
 
-    def _on_update_status(self, event: UpdateStatusEvent) -> None:
+    def _on_update_status(self, _: UpdateStatusEvent) -> None:
         self._update_network_address()
 
     def _on_collect_unit_status(self, event: CollectStatusEvent) -> None:
@@ -100,13 +112,15 @@ class CassandraEvents(Object):
         except ValidationError:
             event.add_status(Status.INVALID_CONFIG.value)
 
-        if self.state.unit.workload_state == "":
+        if self.state.unit.workload_state is None:
             event.add_status(Status.INSTALLING.value)
 
-        if self.state.unit.workload_state == UnitWorkloadState.STARTING.value:
+        if not self.charm.unit.is_leader() and self.state.cluster.state != ClusterState.ACTIVE:
+            event.add_status(Status.WAITING_FOR_CLUSTER.value)
+        elif self.state.unit.workload_state == UnitWorkloadState.STARTING:
             event.add_status(Status.STARTING.value)
 
-        if self.state.unit.workload_state == UnitWorkloadState.ACTIVE.value and (
+        if self.state.unit.workload_state == UnitWorkloadState.ACTIVE and (
             not self.workload.alive() or not self.cluster_manager.is_healthy
         ):
             event.add_status(Status.STARTING.value)
