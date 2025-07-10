6806a3a56fb60aa7e61b1c36b2d227977fdf8bcf
Vladyslav Yurchenko <silvia.homam@gmail.com>
2025-07-10 15:44:17 +0300

    [DPE-7602] Add deploy tls integration test

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -2,16 +2,24 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
 import logging
+from pathlib import Path
+import subprocess
+import jubilant
+from subprocess import PIPE, check_output
 from contextlib import contextmanager
 from typing import Generator
 
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import EXEC_PROFILE_DEFAULT, Cluster, ExecutionProfile, Session
 from cassandra.policies import DCAwareRoundRobinPolicy, TokenAwarePolicy
+import yaml
 
 logger = logging.getLogger(__name__)
 
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = METADATA["name"]
 
 @contextmanager
 def connect_cql(
@@ -43,3 +51,62 @@ def connect_cql(
         yield session
     finally:
         cluster.shutdown()
+
+
+def get_secret_by_label(juju: jubilant.Juju, label: str, owner: str) -> dict[str, str]:
+
+    
+    
+    secrets_meta_raw = juju.cli("list-secrets --format json",include_model=True)
+    secrets_meta = json.loads(secrets_meta_raw)
+    secret_id = None
+
+    for secret_id in secrets_meta:
+        if owner and not secrets_meta[secret_id]["owner"] == owner:
+            continue
+        if secrets_meta[secret_id]["label"] == label:
+            secret_id = secret_id
+            break
+
+    if not secret_id:
+        return dict()
+    
+    secrets_data_raw = juju.cli(f"show-secret --reveal --format json {secret_id}",include_model=True)
+
+    secret_data = json.loads(secrets_data_raw)
+    return secret_data[secret_id]["content"]["Data"]
+        
+
+def check_tls(ip: str, port: int) -> bool:
+    try:
+        result = check_output(
+            f"echo | openssl s_client -connect {ip}:{port}",
+            stderr=PIPE,
+            shell=True,
+            universal_newlines=True,
+        )
+        return bool(result)
+
+    except subprocess.CalledProcessError as e:
+        logger.error(f"command '{e.cmd}' return with error (code {e.returncode}): {e.output}")
+        return False
+
+async def get_address(juju: jubilant.Juju, app_name=APP_NAME, unit_num=0) -> str:
+    """Get the address for a unit."""
+    status = juju.status()
+    address = status.apps[app_name].units[f"{app_name}/{unit_num}"].public_address
+    return address
+    
+def check_node_is_up(juju: jubilant.Juju, unit_num: int, unit_addr: str, app_name: str) -> bool:
+    nd_tool_status_raw = juju.ssh(target=f"{app_name}/{unit_num}", command="sudo snap run charmed-cassandra.nodetool status")
+
+    for line in nd_tool_status_raw.split('\n'):
+        line = line.strip()
+        if unit_addr in line:
+            # Проверяем, начинается ли строка с "UN "
+            if line.startswith('UN '):
+                return True
+            else:
+                return False    
+
+    return False
---
+++ tests/integration/test_tls.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import json
+import logging
+import os
+from pathlib import Path
+from helpers import APP_NAME, check_node_is_up, check_tls, get_address, get_secret_by_label
+import tempfile
+
+import jubilant
+
+TRUSTSTORE_PASSWORD = "truststore-password"
+KEYSTORE_PASSWORD = "keystore-password"
+CLIENT_CA_CERT = "client-ca-cert"
+CLIENT_CERTIFICATE = "client-certificate"
+CLIENT_CHAIN = "client-chain"
+CLIENT_CSR = "client-csr"
+CLIENT_PRIVATE_KEY = "client-private-key"
+PEER_CA_CERT = "peer-ca-cert"
+PEER_CERTIFICATE = "peer-certificate"
+PEER_CHAIN = "peer-chain"
+PEER_CSR = "peer-csr"
+PEER_PRIVATE_KEY = "peer-private-key"
+INTERNAL_PRIVATE_KEY = "internal-ca-key"
+
+PEER_PORT = 7000
+CLIENT_PORT = 9042
+
+def test_deploy_internal_tls(juju: jubilant.Juju, cassandra_charm: Path, app_name: str) -> None:
+    juju.deploy(
+        cassandra_charm,
+        app=app_name,
+        config={"profile": "testing"},
+        num_units=2,
+    )
+    
+    juju.wait(jubilant.all_active)
+
+async def test_default_tls(juju: jubilant.Juju, app_name: str) -> None:
+    unit_addreses = [
+        await get_address(juju=juju, app_name=app_name, unit_num=0),
+        await get_address(juju=juju, app_name=app_name, unit_num=1),
+    ]
+
+    for uaddr in unit_addreses:
+        assert check_tls(
+            ip=uaddr, port=9042
+        )
+
+    # Enshure all nodes are joined to the cluster
+    for i, uaddr in enumerate(unit_addreses):
+        assert check_node_is_up(juju=juju, unit_num=i, unit_addr=uaddr, app_name=app_name)
+
+    for uaddr in unit_addreses:
+        assert check_tls(ip=uaddr, port=PEER_PORT)
+    
+
+def unit_secret_extract(juju: jubilant.Juju, unit_name: str, secret_name: str) -> str | None:
+    user_secret = get_secret_by_label(
+        juju,
+        label=f"cluster.{unit_name.split('/')[0]}.unit",
+        owner=unit_name,
+    )
+
+    return user_secret.get(secret_name)
+
+def app_secret_extract(juju: jubilant.Juju, cluster_name: str, secret_name: str) -> str | None:
+    user_secret = get_secret_by_label(
+        juju,
+        label=f"cluster.{cluster_name}.application",
+        owner=cluster_name,
+    )
+
+    return user_secret.get(secret_name)
+
+
+
