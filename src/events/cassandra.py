78053ec8e0cd9a6ac5f4b072a7e4f6a7120913e9
Vladyslav Yurchenko <silvia.homam@gmail.com>
2025-10-16 16:58:47 +0300

    Fix scale test for github ci/cd

--- src/events/cassandra.py
+++ src/events/cassandra.py
@@ -413,6 +413,7 @@ class CassandraEvents(Object):
         )
 
     def _on_storage_detaching(self, _: StorageDetachingEvent) -> None:
+        logger.info(f"IS_DEC: {self.node_manager.is_bootstrap_decommissioning}")
         if self.node_manager.is_bootstrap_decommissioning:
             logger.warning("Node is already decommissioned")
             return
